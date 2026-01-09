a9b34985693dc1f84701bee8211da314016ff557
Nick Mpamparakos <nikos.mpamparakos77@gmail.com>
2026-01-09 03:43:16 +0200

    check: listener for errors in db

--- config/db.js
+++ config/db.js
@@ -9,4 +9,9 @@ const pool = new Pool({
     connectionTimeoutMillis: 20000,
 });
 
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
 module.exports = pool;
